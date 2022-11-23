c8c7a31c3d1e1441f8e7aa5bfb821b6fc9debc9c
Abdel <113614023+TAHIRI1985@users.noreply.github.com>
2022-11-23 19:52:27 +0100

    .

--- bijouxsiam/src/components/Product.vue
+++ bijouxsiam/src/components/Product.vue
@@ -3,9 +3,9 @@
             <fieldset  class=" col-ms-12 col-md-5 col-lg-3  fieldset" >
              <article class="f1"> 
               
-              <a href="#"><img id="photo1" :src="require(`@/assets/${image}`)" alt="bijoux" height="">  </a>
+              <a href="#"><img id="photo1" :src="require(`@/assets/${image}`)" alt="bijoux" height="">  
               <!-- <img id="photo2" :src="require(`@/assets/${image2}`)" alt="bijoux" height=""></img> -->
-         
+            </a>
              </article >
                 <button  v-if="quantity>0"  @click="quantity -=1 ; count++ ; ajout++ " class=" ajouter" href="#" ><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
                <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5V8z"/>
@@ -27,8 +27,10 @@
                <span v-if="quantity === 0">
                  Rupture de stock
                 </span>
+                <span> 
                 {{totalPrix}}
                 {{ totalProducts }}
+              </span>
             </fieldset>
              <!-- <h4> Total des produits:{{ totalProducts }}</h4> 
             <h5> Total Prix:{{totalPrix}} €</h5>  -->
@@ -55,7 +57,7 @@ export default {
   data(){
           return {
               products:[],
-             
+            
           }
     },
     computed: {
@@ -63,12 +65,12 @@ export default {
       return this.products.reduce((sum,quantity) => {
         // sum += product.quantity
         // return sum
-        return sum + quantity;
+        return sum + product.quantity;
       }, 0);
     },
     totalPrix() {
       return this.products.reduce((total, product) => {
-        return total + product.count *product.prix;
+        return total+ product.count *product.prix;
       }, 0);
     },
   },
@@ -91,14 +93,14 @@ export default {
 article.f1 {
   background-size: 100% 100%;
 }
-#photo2 {
+/* #photo2 {
   z-index: 2;
   margin-top: -350px;
 
   height: 100%;
   width: 100%;
   padding-top: 0px;
-}
+} */
 #photo1 {
   cursor: pointer;
   height: 100%;
